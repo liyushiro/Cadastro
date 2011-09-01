8f92c2b48ebb73823bcc6ca4eb0a71f1b699fe90
Ricardo Chikasawa <liyshiro@gmail.com>
2011-09-01 16:42:42 -0300

    mudando

---
+++ src/com/cadastro/RegCad.java
@@ -0,0 +1,11 @@
+package com.cadastro;
+
+public class RegCad {
+	
+		protected String nome;
+		protected String endereco;
+		protected String telefone;
+		protected RegCad Ant;
+		protected RegCad Prox;
+
+}
